631283a9af2cfb65c38d07e045b0e38d51afe04a
Dogged Mind <39561119+Ibkodus116@users.noreply.github.com>
2022-06-19 10:24:48 +0100

    all done

--- IBK_SME/IBK_SME/settings.py
+++ IBK_SME/IBK_SME/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*^)+7b)53-q&=4re7id&tl^b%7#cvyglyoxfg5(%=9t30q2cq-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
